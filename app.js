29b674dd5336ea491e1449425035ecd8641d222d
MinhajSadik <MinhajoSadik@gmail.com>
2022-09-03 01:01:22 +0600

    [LEARNED] how to scale nodejs project using cluster module

--- app.js
+++ app.js
@@ -1,43 +1,47 @@
 import dotenv from "dotenv";
 import express from "express";
-import redis from "redis";
-import connectDB from "./DB/database.js";
+// import error from "http-errors";
+import cluster from "cluster";
+import morgan from "morgan";
+import os from "os";
+// import connectDB from "./DB/mdb.js";
+import errorMiddleware from "./middlewares/errors.js";
 dotenv.config();
+
+const numOfCPU = os.cpus().length;
 
 // Initialize express app and middleware
 const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-// Connect to Redis
-const redisUrl = process.env.REDIS_PUBLIC_URL;
-const redisPORT = process.env.REDIS_PORT;
-const redisClient = redis.createClient(redisPORT, redisUrl);
-
-// redis client error handling
-redisClient.on("error", (err) => {
-  console.log(err);
-}),
-  // redis client success handling
-  redisClient.on("connect", () => {
-    console.log("Redis client connected");
-  });
-
-await redisClient.connect();
+app.use(morgan("dev"));
 
 // Connect to MongoDB
-connectDB();
+// connectDB();
 
 app.get("/", (req, res) => {
   res.send(`System Desing API Viewer`);
+  cluster.worker.kill();
 });
 
 // Initialize routes
 import usersRouter from "./routes/usersRoute.js";
 app.use("/api/users", usersRouter);
 
-// Start server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+app.use(errorMiddleware);
+
+if (cluster.isPrimary) {
+  for (let i = 0; i < numOfCPU; i++) {
+    cluster.fork();
+  }
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+    cluster.fork();
+  });
+} else {
+  // Start server
+  app.listen(PORT, () => {
+    console.log(`Server is running on pid ${process.pid} & port ${PORT}`);
+  });
+}
