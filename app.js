aa4c756c05b503ecfb4a82f3ae8e7ce0156aeef9
MinhajSadik <MinhajoSadik@gmail.com>
2022-10-01 17:23:57 +0600

    [NOTHING] file name changed

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(express.json());
-app.use(cors({ origin: "*" }));
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 
