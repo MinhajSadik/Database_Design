6ccf216a45a1cc1bf8a9bc49a95959f3c4cfc550
MinhajSadik <MinhajoSadik@gmail.com>
2022-10-01 23:58:37 +0600

    [NOTHING] cors issue

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({ origin: "http://localhost:3000" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 
